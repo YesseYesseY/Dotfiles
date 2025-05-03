5bb828305fc03b06affc2a8e7df146064391f8ac
YesseYesseY <yesseyyesyessey@gmail.com>
2025-05-03 23:13:35 +0200

    Hyprland + Waybar + init.sh

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+for dir in .config/*; do
+    if [ -d "$dir" ]; then
+        if [ -d "$HOME/.config/$(basename $dir)" ]; then
+            if [ -L "$HOME/.config/$(basename $dir)" ]; then
+                rm ~/.config/$(basename $dir)
+            else
+                mv ~/.config/$(basename $dir) ~/.config/old_$(basename $dir)
+            fi
+        fi
+        ln -s $(realpath $dir) ~/.config/$(basename $dir)
+    fi
+done
