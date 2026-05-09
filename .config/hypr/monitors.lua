0cb705e2f43d60be1ed303fd7819b8c256a732b5
YesseYesseY <yesseyyesyessey@gmail.com>
2026-05-10 01:38:39 +0200

    Remove legacy hypr configs

--- .config/hypr/monitors.lua
+++ .config/hypr/monitors.lua
@@ -10,7 +10,7 @@ else
     hl.monitor({
       output = "HDMI-A-1",
       mode = "1920x1080@60",
-      position = "-1920x360",
+      position = "2560x360",
       scale = 1,
     })
     hl.monitor({
@@ -22,7 +22,7 @@ else
     hl.monitor({
       output = "HDMI-A-2",
       mode = "1920x1080@60",
-      position = "2560x360",
+      position = "-1920x360",
       scale = 1,
     })
 end
